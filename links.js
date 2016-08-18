4ff7520e5757854b2882c53bf176ea586385b164
nikhilb99 <nikhilb2199@gmail.com>
2016-08-18 15:30:22 -0400

    Add files via upload

---
+++ links.js
@@ -0,0 +1,11 @@
+links([
+{"display": "Google","url": "https://www.google.com"},
+{"display": "COS", "url": "https://func4eos.kdc.capitalone.com/C1/Login.aspx"},
+{"display": "EASE","url": "https://awscosdev51.kdc.capitalone.com/ease-ui/#/login"},
+{"display": "Yahoo","url": "https://www.yahoo.com"},
+{"display": "Pulse","url": "https://pulse.kdc.capitalone.com/welcome"},
+{"display": "Microsoft","url": "https://www.microsoft.com/en-us/"},
+{"display": "Version One", "url": "https://www.versionone.com"},
+{"display": "Capital One","url": "https://www.capitalone.com/"},
+{"display": "Capital One GitHub","url": "https://github.kdc.capitalone.com/"}
+]);
